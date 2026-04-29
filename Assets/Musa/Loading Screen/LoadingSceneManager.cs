79a4e4ddbb47fee6b2bdd61c6719a28e5ac721e7
Loyldnova47 <mikekhorombimusa@gmail.com>
2026-04-29 12:08:02 +0200

    Deleted Loader
    For the game

--- Assets/Musa/Loading Screen/LoadingSceneManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LoadingSceneManager : MonoBehaviour
-{
-    private void Start()
-    {
-        Loader.LoaderCallback(); // Call the Loader callback to start loading the next scene when the loading screen is active
-    }
-}
