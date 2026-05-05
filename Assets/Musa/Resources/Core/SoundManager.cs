cef47bbc4b0c1f0b56614ea117852b4f932a962c
Loyldnova47 <mikekhorombimusa@gmail.com>
2026-05-05 13:52:52 +0200

    Fixed the dialogue system

--- Assets/Musa/Resources/Core/SoundManager.cs
+++ Assets/Musa/Resources/Core/SoundManager.cs
@@ -5,11 +5,12 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
 
     private AudioSource source;
+   // GameObject pressEtoInteractOBJ;
 
     private void Awake()
     {
-       Instance = this; 
-       source = GetComponent<AudioSource>();
+        Instance = this;
+        source = GetComponent<AudioSource>();
     }
 
     public void PlaySound(AudioClip sound)
@@ -17,5 +18,14 @@ public class SoundManager : MonoBehaviour
         source.PlayOneShot(sound);
     }
 
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
 
+    //    if(collision.CompareTag("PPlayer"))
+    //    {
+    //        pressEtoInteractOBJ.SetGameObjectsActive(true);
+    //    }
+
+
+    //}
 }
