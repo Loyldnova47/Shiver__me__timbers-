eadae747c4adfeb92980a7228191e3d1e1fbfc1f
Loyldnova47 <mikekhorombimusa@gmail.com>
2026-05-05 04:44:45 +0200

    i added an incomplete Dialogue system
    i suck at this

---
+++ Assets/Musa/Resources/Core/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private AudioSource source;
+
+    private void Awake()
+    {
+       Instance = this; 
+       source = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+        source.PlayOneShot(sound);
+    }
+
+
+}
