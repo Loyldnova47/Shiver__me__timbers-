1c3ea1ca4338fb190ed9ddd16d98ba8dd7dcb09a
Loyldnova47 <mikekhorombimusa@gmail.com>
2026-05-28 16:18:10 +0200

    I deleted the Save system
    the tutorial did not work

--- Assets/Musa/Resources/Save system/New Folder/GameState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "GameState", menuName = "ScriptableObject/GameState", order = 1)]
-public class GameState : ScriptableObject
-{
-    public string playerSpawnLoaction = "";
-}
