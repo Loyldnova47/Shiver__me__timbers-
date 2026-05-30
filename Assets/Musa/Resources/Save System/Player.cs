161c0567492196bbc88b9dc0deed01edb417063e
Loyldnova47 <mikekhorombimusa@gmail.com>
2026-05-31 01:38:01 +0200

    Save system
    I have worked on the saving system code, i just need to make it an auto save.

---
+++ Assets/Musa/Resources/Save System/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int level = 3;
+    public int health = 100;
+
+    public void SavePLayer ()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer ()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        level = data.level;
+        health = data.health;
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+}
